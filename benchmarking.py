6285a4766fa625c5e9b9594b913375f606132068
Zak <WRNZAK001@myuct.ac.za>
2021-10-04 17:15:04 +0100

    Benchmarking

---
+++ benchmarking.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timedelta
+from data_source import CSVDataSource
+from main import process_batch
+from reader import readfile
+from encryption import Encryption
+from compression import Compression
+import subprocess
+import os
+from time import time, strftime, localtime
+from datetime import timedelta
+
+
+class Benchmarking:
+    start = ''
+
+    def secondsToStr(elapsed=None):
+        if elapsed is None:
+            return strftime("%Y-%m-%d %H:%M:%S", localtime())
+        else:
+            return str(timedelta(seconds=elapsed))
+
+    def log(s, elapsed=None):
+        line = "="*40
+        print(line)
+        print(Benchmarking.secondsToStr(), '-', s)
+        if elapsed:
+            print("Elapsed time:", elapsed)
+        print(line)
+
+    def startlog():
+        global start
+        start = time()
+        # Benchmarking.log("Starting log")
+
+    def endlog():
+        global start
+        end = time()
+        elapsed = end-start
+        Benchmarking.log("End Program", Benchmarking.secondsToStr(elapsed))
+
+    def endlogThrow():
+        global start
+        end = time()
+        elapsed = end-start
--- test.py
+++ test.py
@@ -4,8 +4,10 @@ from main import process_batch
 from reader import readfile
 from encryption import Encryption
 from compression import Compression
+from benchmarking import Benchmarking
 import subprocess
 import os
+
 
 if __name__ == "__main__":
     current_time = datetime.utcnow()
@@ -16,36 +18,46 @@ if __name__ == "__main__":
         data = data_source.next()
         data.insert(0, current_time + timedelta(seconds=10*i))
         batch_rows.append(data)
-    
+
     encrypted_filename = process_batch(
-        batch_rows=batch_rows, 
-        start_time=current_time, 
+        batch_rows=batch_rows,
+        start_time=current_time,
         end_time=current_time + timedelta(seconds=10*9),
         keep_intermediaries=True
     )
 
-    e = Encryption()
+    print("Compression benchmark:")
+    for i in range(10):
+        Benchmarking.startlog()
+        c = Compression()
+        Benchmarking.endlogThrow()
+    Benchmarking.startlog()
     c = Compression()
+    Benchmarking.endlog()
+
+    print("Encryption benchmark:\n")
+    for i in range(10):
+        Benchmarking.startlog()
+        e = Encryption()
+        Benchmarking.endlogThrow()
+    Benchmarking.startlog()
+    e = Encryption()
+    Benchmarking.endlog()
 
     csv_filename = encrypted_filename.split(".gz.enc")[0]
-    out_filename = os.path.join("".join(csv_filename.split("/")[:-1]), "test.csv").replace(" ", "\ ")
+    out_filename = os.path.join(
+        "".join(csv_filename.split("/")[:-1]), "test.csv").replace(" ", "\ ")
 
     readfile(
-        e, c, 
-        in_filename=encrypted_filename, 
+        e, c,
+        in_filename=encrypted_filename,
         out_filename=out_filename
     )
 
     print(f"diff \"{csv_filename}\" {out_filename}")
-    process = subprocess.run(f"diff \"{csv_filename}\" {out_filename}", shell=True)
+    process = subprocess.run(
+        f"diff \"{csv_filename}\" {out_filename}", shell=True)
     if process.returncode == 0:
         print("Files match")
     else:
         print("Files don't match")
-    
-
-
-
-
-
-    
